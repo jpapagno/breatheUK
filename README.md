3ece8436cab402873e40d19bc8da8c370e68093f
jpapagno <100060927+jpapagno@users.noreply.github.com>
2022-08-04 13:52:43 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-app
+React frontend for our London Air Pollution app!
+
+hello this is my professional message. -joey
+hehe i edited this
+this is connie again!
+hi -carla
+gina's here >:))
+JOHN!!!!!!!!!!
+JOHN x2
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+
+const Home = () => {
+
+    
+    return (  
+        <div className="home">
+            <h2>
+                Homepage
+            </h2>
+            <span role="img" aria-label="nerd">🤓🤓🤓🤓🤓🤓</span>   
+        </div>
+    );
+}
+ 
+export default Home;
